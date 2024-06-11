b5915a1e7ce7553c6f140c7eab97e4df1374c34a
Derek Detweiler <derek.detweiler@gmail.com>
2024-06-11 13:55:28 -0400

    Updated elevenlabs and polly to perform diff check by default.

--- src/parsers/polly.js
+++ src/parsers/polly.js
@@ -1,6 +1,27 @@
 const
-    getFiles = require('../helpers/getFiles'),
+    fs = require('fs').promises,
     getJSON = require('../helpers/getJSON'),
+    matches = (file, types) => types.reduce((prev, current) => prev || (file.slice(-(current.length + 1)) === `.${current}`), false),
+    getFiles = async ({folder = './', compareAgainst = null, fileTypes = []}) => {
+        const
+            all = (await fs.readdir(folder))
+                .filter((file) => (file.indexOf('.') !== 0) && (fileTypes.length === 0 || matches(file, fileTypes)));
+
+        if (compareAgainst) {
+            const
+                list = (Array.isArray(compareAgainst) ? compareAgainst : Object.keys(compareAgainst)).map((id) => `${id}.mp3`);
+
+            return {
+                missing: list.filter((id) => all.indexOf(id) === -1),
+                unlisted: all.filter((id) => list.indexOf(id) === -1),
+                all
+            };
+        }
+
+        return {
+            all
+        };
+    },
     parsePolly = async ({archive, config, difference}) => {
         const
             {output, script, files = {}} = config,
@@ -16,15 +37,28 @@ const
 
         if (difference) {
             const
-                {missing} = await getFiles({
+                {missing, unlisted} = await getFiles({
                     compareAgainst: cfg.files,
+                    fileTypes: ['mp3'],
                     folder: output
                 });
 
             cfg.files = missing.reduce((obj, file) => {
-                obj[file] = cfg.files[file];
+                const
+                    id = file.substring(0, file.length - 4);
+
+                obj[id] = cfg.files[id];
                 return obj;
             }, {});
+
+            for (let i = 0; i < unlisted.length; i++) {
+                await fs.rm(`${output}${unlisted[i]}`);
+                console.log(`Removed "${unlisted[i]}"`);
+            }
+
+            if (missing.length === 0) {
+                throw Error('All voice-over files already exist.');
+            }
         }
 
         archive.append(JSON.stringify(cfg, null, 4), {name: 'polly.json'});
